a09243935a3898f2fe9f51843f3b42c501e8c4e5
Muhammad Zufarrifqi Prakoso <77829361+zrifqip@users.noreply.github.com>
2021-04-23 23:05:57 +0700

    Update and rename Tugas Opsional (Belum Selesai).py to Tugas Opsional.py

---
+++ Tugas Opsional.py
@@ -0,0 +1,56 @@
+import numpy as np
+from cv2 import cv2 as cv
+
+def redContours (rect,frame) :
+        lower_red = np.array([0,50,50])
+        upper_red = np.array([10,255,255])
+        mask2 = cv.inRange(rect, lower_red, upper_red)
+        redcnts= cv.findContours(mask2,
+                                cv.RETR_TREE,
+                                cv.CHAIN_APPROX_SIMPLE)[-2]
+        if(len(redcnts) > 0) :
+            red_area = max(redcnts, key=cv.contourArea)
+            (x, y, w, h) = cv.boundingRect(red_area)
+            imageFrame = cv.rectangle(frame, (x, y), 
+                                    (x + w, y + h), 
+                                    (0, 0, 255), 2)
+              
+            cv.putText(imageFrame, "Red Colour", (x, y),
+                        cv.FONT_HERSHEY_DUPLEX, 1.0,
+                        (0, 0, 255))
+
+cap = cv.VideoCapture(0)
+
+while 1:
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([100,150,0])
+    upper_blue = np.array([140,255,255])
+    # Threshold the HSV image to get only blue colors
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    bluecnts = cv.findContours(mask,
+                              cv.RETR_EXTERNAL,
+                              cv.CHAIN_APPROX_SIMPLE)[-2]
+    if len(bluecnts) > 0:
+        blue_area = max(bluecnts, key=cv.contourArea)
+        (x, y, w, h) = cv.boundingRect(blue_area)
+        imageFrame = cv.rectangle(frame, (x, y), 
+                                (x + w, y + h), 
+                                (0, 255, 0), 2)
+        cv.putText(imageFrame, "Warna Biru", (x, y),
+                        cv.FONT_HERSHEY_DUPLEX, 1.0,
+                        (0, 255, 0))
+        rect = hsv[y:y+h,x:x+w]
+        redContours(rect,imageFrame)
+
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+
+     
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
